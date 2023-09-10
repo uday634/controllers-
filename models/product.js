a1e522cada113b0a568965b3f99ec78ebc22e14c
uday634 <uk8096386837@gmail.com>
2023-09-10 17:01:50 +0530

    optimized the module code

--- models/product.js
+++ models/product.js
@@ -2,33 +2,34 @@ const fs = require('fs')
 const path = require('path');
 const rootDir = require('../util/path');
 const { json } = require('body-parser');
+const p = path.join(rootDir, 'data', 'products.json');
+const getProductsFromFile = (cb)=>{
+    
+        fs.readFile(p, (err, data)=>{
+            if(err){
+                cb([]);
+            }else{
+                cb(JSON.parse(data))
+            }
+        })
+    }
+
 module.exports = class Product {
     constructor(t){
         this.title = t;
     }
 
     save(){
-        const p = path.join(rootDir, 'data', 'products.json');
-        fs.readFile(p, (err, data)=>{
-            let products = [];
-            if(!err){
-                products= JSON.parse(data);
-            }
+        getProductsFromFile(products=>{
             products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err)=>{
+            fs.writeFile(p, JSON.stringify(products),err=>{
                 console.log(err)
             });
         });
     }
 
     static fetchAll(cb){
-        const p = path.join(rootDir, 'data', 'products.json');
-        fs.readFile(p, (err, data)=>{
-            if(err){
-                cb([]);
-            }
-            cb (JSON.parse(data))
-        })
+        getProductsFromFile(cb);
     }
 
 }
